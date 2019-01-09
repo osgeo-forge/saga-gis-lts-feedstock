3798229b02c979084eb75b9dd5656c89f5be4d01
Larry Shaffer <lshaffer@boundlessgeo.com>
2019-01-09 00:59:21 -0700

    Rename osgeo-env.bat to osgf_env.bat

--- recipe/saga_cmd.bat
+++ recipe/saga_cmd.bat
@@ -3,7 +3,7 @@
 for %%a in (%~dp0\..) do set LIBRARY_PREFIX=%%~fa
 
 :: Override with osgeo-forge env
-if exist "%~dp0\osgeo-env.bat" call "%~dp0\osgeo-env.bat"
+if exist "%~dp0\osgf_env.bat" call "%~dp0\osgf_env.bat"
 
 set "SAGA_MLB=%LIBRARY_PREFIX%\apps\saga-gis-lts;%LIBRARY_PREFIX%\bin;%LIBRARY_PREFIX%\lib\vc_x64_dll"
 set "PATH=%LIBRARY_PREFIX%\lib\vc_x64_dll;%PATH%"
